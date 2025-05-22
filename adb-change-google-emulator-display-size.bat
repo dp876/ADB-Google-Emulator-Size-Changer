c4372722de0f91583a1ca3700f3738a89351f13b
Damain Patterson <damainp13@gmail.com>
2025-05-22 11:48:46 -0500

    - update options to change font size

--- adb-change-google-emulator-display-size.bat
+++ adb-change-google-emulator-display-size.bat
@@ -17,12 +17,13 @@ echo ^|   Google Emulator Size Changer    ^|
 echo ^|            by dvp876              ^|
 echo *-----------------------------------*
 echo ^|  1. Connect and Apply Defaults    ^|
+echo ^|     - screen size: 1080x2400      ^|
+echo ^|     - screen density: 440         ^|
 echo ^|  2. Connect to Default Device     ^|
-echo ^|  3. Set screen size to 1080x2400  ^|
-echo ^|  4. Set screen density to 440     ^|
-echo ^|  5. Set custom screen size        ^|
-echo ^|  6. Set custom screen density     ^|
-echo ^|  7. Reset display                 ^|
+echo ^|  3. Set font size                 ^|
+echo ^|  4. Set custom screen size        ^|
+echo ^|  5. Set custom screen density     ^|
+echo ^|  6. Reset display                 ^|
 echo ^|  0. Exit                          ^|
 echo *-----------------------------------*
 echo.
@@ -33,14 +34,12 @@ if "%choice%"=="1" (
 ) else if "%choice%"=="2" (
     goto CONNECT
 ) else if "%choice%"=="3" (
-    goto SCREEN_SIZE
+    goto FONT_SIZE
 ) else if "%choice%"=="4" (
-    goto DENSITY
-) else if "%choice%"=="5" (
     goto CUSTOM_SCREEN_SIZE
-) else if "%choice%"=="6" (
+) else if "%choice%"=="5" (
     goto CUSTOM_DENSITY
-) else if "%choice%"=="7" (
+) else if "%choice%"=="6" (
     goto RESET
 ) else if "%choice%"=="0" (
     exit
@@ -74,16 +73,35 @@ pause
 cls
 goto MENU
 
+:FONT_SIZE
+echo *-----------------------------------*
+echo ^|  Screen Size Options              ^|
+echo *-----------------------------------*
+echo ^|  1. Small: 0.85                   ^|
+echo ^|  2. Default: 1.0                  ^|
+echo ^|  3. Large: 1.15                   ^|
+echo ^|  4. Largest: 1.3                  ^|
+echo ^|  5. Extra: 2.0                     ^|
+echo *-----------------------------------*
+echo.
+set /p option="Select Size: "
 
-:SCREEN_SIZE
-adb shell wm size 1080x2400
-pause
-cls
-goto MENU
+if "%option%"=="1" (
+    set font_size=0.85
+) else if "%option%"=="2" (
+    set font_size=1.0
+) else if "%option%"=="3" (
+    set font_size=1.15
+) else if "%option%"=="4" (
+    set font_size=1.3
+) else if "%option%"=="5" (
+    set font_size=2.0
+) else (
+    echo Invalid option. Using default size 1.0
+    set font_size=1.0
+)
 
-
-:DENSITY
-adb shell wm density 440
+adb shell settings put system font_scale %font_size%
 pause
 cls
 goto MENU
